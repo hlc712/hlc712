c57c62e6753c0168973ce2668b639a99f7016b0a
hlc712 <74356598+hlc712@users.noreply.github.com>
2020-11-14 20:06:58 +0800

    Create 015.20201114.c

---
+++ 015.20201114.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+	int firstnum=12;
+	int lastnum=14;
+	printf("%d\n",firstnum+lastnum);
+	printf("%d\n",firstnum-lastnum);
+    printf("%d\n",firstnum*lastnum);
+    printf("%d\n",firstnum/lastnum);
+
+
+}
