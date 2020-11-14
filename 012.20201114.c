a9fe475ac86a9c98dc3767f5464d571bfd646881
hlc712 <74356598+hlc712@users.noreply.github.com>
2020-11-14 19:51:30 +0800

    Create 012.20201114.c

---
+++ 012.20201114.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+	int n=20201114;
+	printf("*****深夜的酒不上清晨的粥！*****\n");
+	printf("    *****              *****    \n");
+    printf("        ****        ****        \n");
+    printf("           ***    ***           \n");
+	printf("               ***              \n");
+	printf("%d\n",n);
+
+}
