c44c466277badcd57e7d2894a9c2bd29152e8221
hlc712 <74356598+hlc712@users.noreply.github.com>
2020-11-19 19:51:03 +0800

    Create 111920.★.c

---
+++ 111920.★.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+	int a=20201119;
+	printf("               ★                 \n");
+	printf("              ★★                \n");
+	printf("            ★★★★              \n");
+	printf("          ★★★★★★            \n");
+	printf("        ★★★★★★★★          \n");
+	
+}
