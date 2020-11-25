8e11d30e289b53494b06e6980df2bb7ca158bd88
hlc712 <74356598+hlc712@users.noreply.github.com>
2020-11-25 09:53:11 +0800

    Create 112502.qs大学排名查询.app.c

---
+++ 112502.qs大学排名查询.app.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+void main()
+{
+	int i; 
+	scanf("%d",&i);
+	if(i==1)
+	{
+		printf("清华大学");
+	}
+	else if(i==2)
+	{
+		printf("北京大学");
+	}
+	else if(i==3)
+	{
+		printf("浙江大学");
+	}
+	else if(i==4)
+	{
+		printf("上海交通大学");
+	}
+	else if(i==5)
+	{
+		printf("南京大学");
+	}
+	else if(i==6)
+	{
+		printf("复旦大学");
+	}
+	else if(i==7)
+	{
+		printf("中国科技大学");
+	}
+	else if(i==8)
+	{
+		printf("华中科技大学");
+	}
+	else if(i==9)
+	{
+		printf("武汉大学");
+	}
+	else if(i==10)
+	{
+		printf("西安交通大学");
+	}
+	else
+	{
+		printf("老铁的牌名太靠后了，请输入1—10！");
+	}
+	
+
+
+}
