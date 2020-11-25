6da1068a4359c1ccebeb0996fcfc9de13d8f4e45
hlc712 <74356598+hlc712@users.noreply.github.com>
2020-11-25 16:59:10 +0800

    Create 112503.模拟延时程序.app.c

---
+++ 112503.模拟延时程序.app.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<windows.h>
+void main()
+{
+	printf("天气冷了，有句成语叫做忘穿秋裤，今天你穿秋裤了吗？\n");
+	Sleep(3000);
+	printf("...\n");
+	printf("我重来不穿秋裤，作为广西人不需要秋裤这种东西\n");
+	Sleep(1000);
+	printf("...\n");
+
+}
