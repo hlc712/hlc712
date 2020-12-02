f05d2e5d7d0d5edafe95cb6788d6787734666db4
hlc712 <74356598+hlc712@users.noreply.github.com>
2020-12-02 08:57:20 +0800

    Create 120201.switch语句.app.c

---
+++ 120201.switch语句.app.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{ 
+	int a=5;
+	switch(a)
+	{
+	case 1:printf("清华大学");break;
+	case 2:printf("北京大学");break;
+	case 3:printf("复旦大学");break;
+	case 4:printf("中国科技大学");break;
+	case 5:printf("浙江大学");break;
+	case 6:printf("上海交通大学");break;
+	case 7:printf("南京大学");break;
+	case 8:printf("中山大学");break;
+	case 9:printf("武汉大学");break;
+	case 10:printf("哈尔滨工业大学");break;
+	case 11:printf("西安交通大学");break;
+	case 12:printf("同济大学");break;
+	case 13:printf("北京师范大学");break;
+	case 14:printf("中国人民大学");break;
+	case 15:printf("南开大学");break;
+	case 16:printf("上海大学");break;
+	case 17:printf("华中科技大学");break;
+	case 18:printf("吉林大学");break;
+	case 19:printf("北京理工大学");break;
+	case 20:printf("北京航空航天大学");break;
+	default:printf("a为其它值");	
+	}
+}
